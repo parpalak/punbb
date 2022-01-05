9b26ea49cb3ea5556d537cf8b0eb3ede3d44b164
Roman Parpalak <roman@parpalak.com>
2022-01-05 12:48:55 +0300

    Fixed preg_replace with e modifier

--- extensions/pun_pm/functions.php
+++ extensions/pun_pm/functions.php
@@ -647,10 +647,15 @@ function pun_pm_preview($receiver, $subject, $body, &$errors)
 
 function pun_pm_next_reply($str)
 {
-	if (substr($str, 0, 4) == 'Re: ')
-		return 'Re[2]: ' . substr($str, 4);
-	$str1 = preg_replace('#^Re\[(\d{1,10})\]: #eu', '\'Re[\'.(\\1 + 1).\']: \'', $str);
-	return $str == $str1 ? 'Re: ' . $str : $str1;
+	if (substr($str, 0, 4) === 'Re: ') {
+        return 'Re[2]: ' . substr($str, 4);
+    }
+
+	$result = preg_replace_callback('#^Re\[(\d{1,10})\]: #u', static function ($m) {
+	    return 'Re['.($m[1] + 1). ']: ';
+    }, $str);
+
+	return $str === $result ? 'Re: ' . $str : $result;
 }
 
 function pun_pm_inbox()

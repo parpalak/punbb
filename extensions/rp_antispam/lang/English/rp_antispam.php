1926dba56a00b415ca40fb0bdeb41cc0b280a740
Roman Parpalak <roman@parpalak.com>
2022-01-05 12:27:15 +0300

    Added extensions to the repo.

---
+++ extensions/rp_antispam/lang/English/rp_antispam.php
@@ -0,0 +1,12 @@
+<?php
+
+// Language definitions used in rp_antispam
+
+$lang_rp_antispam = array(
+	'Name'							=>	'Name',
+	'Name Info'						=>	'Keep it empty, it\'s for stupid robots.',
+	'Question'						=>	'How much is %s?',
+	'Question Info'					=>	'Quite a simple question, isn\'t it?',
+	'Invalid Answer'				=>	'Seems like you don\'t answered the question right. Try again',
+	'Invalid Text'					=>	'You entered something in the "Name" field. Please, keep it empty.',
+);
---
+++ extensions/rp_antispam/lang/Russian/rp_antispam.php
@@ -0,0 +1,12 @@
+<?php
+
+// Language definitions used in rp_antispam
+
+$lang_rp_antispam = array(
+	'Name'							=>	'Имя',
+	'Name Info'						=>	'Оставьте поле пустым, оно для глупых роботов.',
+	'Question'						=>	'Сколько будет %s?',
+	'Question Info'					=>	'Простой пример, не так ли?',
+	'Invalid Answer'				=>	'Кажется, вы решили пример неправильно.',
+	'Invalid Text'					=>	'Вы что-то ввели в поле «Имя». Оставьте его пустым.',
+);
